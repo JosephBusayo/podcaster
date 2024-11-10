1bd6348e506a4050bc053f8c168cbd83ba851488
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2024-11-10 18:09:01 +0100

    setup up file structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,7 +6,6 @@ const Home = () => {
     <div className='mt-9 flex flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending Podcats</h1>
-        <Button variant="outline">Button</Button>
       </section>
     </div>
   )
